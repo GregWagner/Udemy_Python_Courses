bc4f55dc468ca8abfb2563577bb3ddbb51188cfa
gwagner <theGreg101@gmail.com>
2023-11-09 09:45:16 -0600

    Add AI for Simple Games

---
+++ 09_Artifical_Intelligence_for_Simple_Games/Section_04_Genetic_Algorithms/train.py
@@ -0,0 +1,10 @@
+# Space Discover: Genetic Algorithms - Training File 
+
+import numpy as np
+from environment import Environment
+
+# Creating the bots
+# dna = 3, 1, 2, 0
+# fitness = total distance traveled
+class Route():
+    def __init__(self, dnaLength):
